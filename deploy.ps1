ed1233e4c0efe12f5c74b51e404dae70ff8b11ae
Dan Bowker <dan.bowker@googlemail.com>
2019-03-07 06:57:11 +0000

    Refied deployment

--- deploy.ps1
+++ deploy.ps1
@@ -1,3 +1,4 @@
+# Requires Posh-SSH. PS> Install-Module -Name Posh-SSH -RequiredVersion 2.0.2
 $server = 'codeguidelines.hta.cloud'
 $remoteFolder = '/var/www'
 $localFolder = '_site'
@@ -7,5 +8,5 @@ $credentials = Import-Clixml -Path '.\ServerCred.xml'
 $session = New-SSHSession -ComputerName $server -Credential $credentials -AcceptKey
 $null = Invoke-SSHCommand -SSHSession $session -Command "rm -rf $remoteFolder/*"
 Set-SCPFolder -ComputerName $server -Credential $credentials -AcceptKey -LocalFolder $localFolder -RemoteFolder "$remoteFolder"
-$null = Invoke-SSHCommand -SSHSession $session -Command "chmod -R 777 $remoteFolder/."
+$null = Invoke-SSHCommand -SSHSession $session -Command "chmod -R 755 $remoteFolder/."
 Remove-SSHSession -SSHSession $session
